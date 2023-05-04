44e4843dc31f6a94c0b2c4dbd086d8c8c6e25f6a
Angelo Ricardo <angeloengcomp@gmail.com>
2023-05-04 01:22:25 -0300

    feat: add dynamic form in client

--- client/src/pages/RegisterAndLoginForm.tsx
+++ client/src/pages/RegisterAndLoginForm.tsx
@@ -2,17 +2,21 @@ import { api } from '@services/axios';
 import userStore from '@stores/userStore';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
-export function Register() {
-  const { setUsername, setId } = userStore();
+import { Icon } from '@iconify/react';
+export function RegisterAndLoginForm() {
   const [nickname, setNickname] = React.useState('');
   const [password, setPassword] = React.useState('');
   const navigate = useNavigate(); // navigate('/')
+  const [tipeForm, setTipeForm] = React.useState('login'); // login or register
 
   async function register(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    if (!nickname || !password) {
+      return;
+    }
+    const url = tipeForm === 'login' ? '/login' : '/register';
     try {
-      const response = await api.post('/register', {
+      const response = await api.post(url, {
         username: nickname,
         password,
       });
@@ -22,8 +26,12 @@ export function Register() {
     }
   }
   return (
-    <div className="flex h-screen items-center justify-center bg-blue-50">
-      <form className="w-64" onSubmit={register}>
+    <div className="flex h-screen flex-col items-center justify-center bg-blue-50">
+      <h1 className="text-xl font-bold flex items-center gap-1">
+        <Icon icon="mdi:chat-processing"/>
+        MERN CHAT
+      </h1>
+      <form className="my-5 w-64" onSubmit={register}>
         <input
           type="text"
           className="mb-2 block w-full rounded-md border p-2"
@@ -39,9 +47,32 @@ export function Register() {
           onChange={(e) => setPassword(e.target.value)}
         />
         <button className="w-full rounded-md border bg-blue-400 p-2 text-white hover:bg-blue-500 active:bg-blue-700">
-          Register
+          {tipeForm === 'login' ? 'Login' : 'Register'}
         </button>
       </form>
+      {tipeForm === 'login' ? (
+        <div>
+          Don't a Member?{' '}
+          <button
+            onClick={() => {
+              setTipeForm('register');
+            }}
+          >
+            Register Here
+          </button>
+        </div>
+      ) : (
+        <div>
+          Already a Member?{' '}
+          <button
+            onClick={() => {
+              setTipeForm('login');
+            }}
+          >
+            Login Here
+          </button>
+        </div>
+      )}
     </div>
   );
 }
--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,7 +1,7 @@
 import { useUserContext } from '@context/UserContextProvider';
 import PageNotFound from '@pages/404';
 import { FormExample } from '@pages/Form';
-import { Register } from '@pages/Register';
+import { RegisterAndLoginForm } from '@pages/RegisterAndLoginForm';
 import React, { useEffect } from 'react';
 import {
   Routes as Routing,
@@ -23,7 +23,7 @@ export function Routes() {
   return (
     <Routing>
       {/* remove this route to start */}
-      <Route path="/" element={<Register />} />
+      <Route path="/" element={<RegisterAndLoginForm />} />
       {/* <Route path="/" element={<Home />} /> */}
       <Route path="/contato" element={<FormExample />} />
       <Route path="*" element={<PageNotFound />} />
