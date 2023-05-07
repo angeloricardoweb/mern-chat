4137f01e01685a92200c66ed749ff7f6df507247
Angelo Ricardo <angeloengcomp@gmail.com>
2023-05-07 17:47:33 -0300

    feat: configure web socket

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const { User } = require("./src/models/User");
-
+const ws = require("ws");
 const bcryptSalt = bcrypt.genSaltSync(10);
 
 dotenv.config();
@@ -40,15 +40,19 @@ app.post("/login", async (req, res) => {
     if (!passwordValid) {
       return res.status(401).json({ message: "Invalid credentials" });
     } else {
-      jwt.sign({ userId: user._id }, process.env.JWT_SECRET, (err, token) => {
-        if (err) {
-          return res.status(500).json({ message: "Something went wrong" });
+      jwt.sign(
+        { userId: user._id, username: user.username },
+        process.env.JWT_SECRET,
+        (err, token) => {
+          if (err) {
+            return res.status(500).json({ message: "Something went wrong" });
+          }
+          return res
+            .cookie("token", token)
+            .status(200)
+            .json({ token, username: user.username, id: user._id });
         }
-        return res
-          .cookie("token", token)
-          .status(200)
-          .json({ token, username: user.username, id: user._id });
-      });
+      );
     }
   }
 });
@@ -79,7 +83,7 @@ app.post("/register", async (req, res) => {
     });
 
     jwt.sign(
-      { userId: createdUser._id },
+      { userId: createdUser._id, username: createdUser.username },
       process.env.JWT_SECRET,
       (err, token) => {
         if (err) {
@@ -93,6 +97,36 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.listen(4000, () => {
+const server = app.listen(4000, () => {
   console.log("server is running on port 4000");
 });
+
+const wss = new ws.WebSocketServer({ server });
+wss.on("connection", (connection, req) => {
+  const token = returnTokenFromCookiesString(req.headers.cookie);
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, {}, (error, userData) => {
+      if (error) throw error;
+      const { userId, username } = userData;
+      connection.userId = userId;
+      connection.username = username;
+    });
+  }
+});
+
+function returnTokenFromCookiesString(cookies) {
+  return cookies
+    .split("; ")
+    .map((cookie) => {
+      const [key, value] = cookie.split("=");
+      return { key, value };
+    })
+    .filter((cookie) => cookie.key === "token")[0].value;
+}
+
+function returnTokenFromCookiesStringTwo(cookies) {
+  return cookies
+    .split("; ")
+    .find((string) => string.startsWith("token"))
+    .split("=")[1];
+}
--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,17 +1,27 @@
 import { Icon } from '@iconify/react';
+import { useEffect, useState } from 'react';
 
 export function Chat() {
+  const [ws, setWs] = useState<WebSocket | null>(null);
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:4000');
+    setWs(ws);
+    ws.onopen = () => {
+      console.log('connected with websocket server');
+    };
+    ws.onmessage = (e) => {
+      console.log(e);
+    }
+  }, []);
   return (
     <div className="flex h-screen w-full">
-      <div className="w-1/3 bg-white p-2">
-        contacts
-      </div>
+      <div className="w-1/3 bg-white p-2">contacts</div>
       <div className="flex w-2/3 flex-col bg-blue-100 p-2">
         <div className="flex-grow border">msgs</div>
         <div className="flex gap-2">
           <input
             type="text"
-            className="block flex-grow rounded-md rounded-md border p-2"
+            className="block flex-grow rounded-md border p-2"
             placeholder="Type your message here"
           />
           <button className="rounded-md bg-brand-blue-100 p-5 hover:bg-blue-400 active:bg-blue-600">
